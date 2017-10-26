5989030a9cfe6c86851fdac47c828553bb9a014d
Bruno Tuy <murta@tuy.com.br>
2017-10-25 22:38:41 -0300

    component mainButtons

---
+++ src/js/mainButtons.js
@@ -0,0 +1,14 @@
+let _html = '';
+	_html += '<button type="button" class="btn" onclick="mainButtons.button( 4 )"><span class="glyphicon glyphicon-triangle-left"></span></button>';
+	_html += '<button type="button" class="btn" onclick="mainButtons.button( 3 )"><span class="glyphicon glyphicon-home"></span></button>';
+	_html += '<button type="button" class="btn" onclick="mainButtons.button( \'KEYCODE_APP_SWITCH\' )"><span class="glyphicon glyphicon-unchecked"></span></button>';
+	_html += '<button type="button" class="btn" onclick="mainButtons.button( 82 )"><span class="glyphicon glyphicon-cog"></span></button>';
+	_html += '<button type="button" class="btn" onclick="mainButtons.button( 26 )"><span class="glyphicon glyphicon-off"></span></button>';
+
+const _button = ( cod ) => shellCmd( 'input keyevent '+cod );
+
+module.exports = {
+	name: 'Main Buttons',
+	html: _html,
+	button: _button
+};
