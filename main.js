b9b9600b2e843c61d05c1c28fbd85fbbc8cdb8f2
Bruno Tuy <murta@tuy.com.br>
2017-11-21 09:59:50 -0300

    indent tab

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-const {app, BrowserWindow} = require('electron');
+const {
+	app,
+	BrowserWindow
+} = require('electron');
 
 let win;
 
 app.on('ready', () => {
-  win = new BrowserWindow({
-    width: 600,
-    height: 800
-  });
+	win = new BrowserWindow({
+		width: 600,
+		height: 800
+	});
 
-  win.setMenu(null);
-  win.loadURL(`file://${__dirname}/src/index.html`);
-  win.on('closed', () => {
-    win = null;
-  });
+	win.setMenu(null);
+	win.loadURL(`file://${__dirname}/src/index.html`);
+	win.on('closed', () => {
+		win = null;
+	});
 
-  win.webContents.on('devtools-opened', () => {
-    win.webContents.closeDevTools();
-  });
+	win.webContents.on('devtools-opened', () => {
+		win.webContents.closeDevTools();
+	});
 
 }).on('all-window-closed', () => {
-  app.quit();
+	app.quit();
 
 });
