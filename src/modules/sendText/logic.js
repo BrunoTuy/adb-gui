23b7d5c0c0a069eb73a874a77863fa61e533529f
Bruno Tuy <bruno.tuy@pixeon.com>
2018-09-04 16:00:31 -0300

    modulo para envio de texto

---
+++ src/modules/sendText/logic.js
@@ -0,0 +1,17 @@
+const _button = obj => {
+	if ( obj.nodeName != 'BUTTON' || obj.parentNode.nodeName != 'DIV' )
+		return;
+
+	const divComponent = obj.parentNode.parentNode;
+
+	for ( let x = 0; x < divComponent.childNodes.length; x++ )
+		if ( divComponent.childNodes.item(x).nodeName == 'DIV' )
+			for ( let w = 0; w < divComponent.childNodes.item(x).childNodes.length; w++ )
+				if ( divComponent.childNodes.item(x).childNodes.item(w).nodeName == 'INPUT' )
+					shellCmd( `input text ${divComponent.childNodes.item(x).childNodes.item(w).value}`, alert );
+};
+
+module.exports = {
+	name: 'Send Text',
+	button: _button
+};
